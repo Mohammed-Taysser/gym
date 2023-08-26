078f0eac4b88d73d07794c55bed19bad4b6172d6
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-08-26 10:30:52 +0300

    add types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+type SkeletonVariant = 'category' | exercise;
+
+type BodyParts = string[];
+
+type TargetMuscles = string[];
+
+type Equipments = string[];
+
+interface Exercise {
+  bodyPart: string;
+  equipment: string;
+  gifUrl: string;
+  id: string;
+  name: string;
+  target: string;
+}
