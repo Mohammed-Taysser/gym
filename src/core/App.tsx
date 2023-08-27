bbdf870146c083c9d5b4c7f0cb7c8574c5667a04
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-08-27 19:22:51 +0300

    add redux to app

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,14 +1,18 @@
 import { ThemeProvider } from '@mui/material';
 import RouterProvider from '../providers/RouterProvider';
 import themeOptions from './theme';
+import store from '../redux/store';
+import { Provider } from 'react-redux';
 
 import '../assets/css/app.css';
 
 function App() {
   return (
-    <ThemeProvider theme={themeOptions}>
-      <RouterProvider />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={themeOptions}>
+        <RouterProvider />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
