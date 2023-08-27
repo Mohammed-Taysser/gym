5791b089fa6829adf242ff5a0944f2dbef5f86dd
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-08-27 19:22:22 +0300

    fetch exercises only on page load

--- src/layout/Base.tsx
+++ src/layout/Base.tsx
@@ -1,14 +1,24 @@
 import { CircularProgress } from '@mui/material';
 import { Suspense } from 'react';
+import { useDispatch } from 'react-redux';
 import { Outlet } from 'react-router-dom';
+import AsyncWrapper from '../containers/AsyncWrapper';
+import { getExercises } from '../core/API';
+import { initApiCall } from '../redux/exercises.slice';
 
 function BaseLayout() {
+  const dispatch = useDispatch();
+
+  const onExercisesFetch = (exercises: Exercise[]) => {
+    dispatch(initApiCall(exercises));
+  };
+
   return (
-    <div>
+    <AsyncWrapper<Exercise[]> apiCall={getExercises} setData={onExercisesFetch}>
       <Suspense fallback={<CircularProgress />}>
         <Outlet />
       </Suspense>
-    </div>
+    </AsyncWrapper>
   );
 }
 
