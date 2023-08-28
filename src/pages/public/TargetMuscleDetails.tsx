e8069fd0e09c61fe16c7f44ff587cd52b12391e9
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-08-28 17:32:23 +0300

    create target muscles details page

---
+++ src/pages/public/TargetMuscleDetails.tsx
@@ -0,0 +1,50 @@
+import { Alert, Container, Typography } from '@mui/material';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import Banner from '../../components/Banner';
+import ExercisesGrid from '../../components/grids/Exercises.grid';
+import { getExercisesByTargetMuscle } from '../../redux/exercises.slice';
+import { RootStoreState } from '../../redux/store';
+
+function TargetMuscleDetails() {
+  const { title = '' } = useParams();
+  const dispatch = useDispatch();
+  const exercises =
+    useSelector((state: RootStoreState) => state.api.exerciseBy.target) ?? [];
+
+  useEffect(() => {
+    dispatch(getExercisesByTargetMuscle(title));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [title]);
+
+  return (
+    <>
+      <Banner>
+        <Typography fontWeight={700} variant='h3' textAlign='center'>
+          <Link className='reset-link' to='/exercises'>
+            Exercises
+          </Link>{' '}
+          /{' '}
+          <Typography
+            display='inline-block'
+            fontSize='inherit'
+            fontWeight='inherit'
+            textTransform='capitalize'
+            color='primary'
+          >
+            {title}
+          </Typography>
+        </Typography>
+      </Banner>
+
+      <Container maxWidth='md' sx={{ my: 15 }}>
+        {!title && <Alert severity='error'>No title Provide</Alert>}
+
+        <ExercisesGrid exercises={exercises} hide={['target']} />
+      </Container>
+    </>
+  );
+}
+
+export default TargetMuscleDetails;
