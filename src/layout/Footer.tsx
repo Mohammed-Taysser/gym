f11698fbb01607807a21c24ebebe5104f8f8570f
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-08-28 17:40:43 +0300

    create footer layout

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,21 @@
+import { Box, Stack, Typography } from '@mui/material';
+import logo from '../assets/images/icons/logo-full.png';
+
+function Footer() {
+  return (
+    <Box mt='80px' bgcolor='#fff' pt={3}>
+      <Stack alignItems='center'>
+        <img
+          src={logo}
+          alt='favicon full'
+          style={{ width: '200px', height: '41px' }}
+        />
+      </Stack>
+      <Typography variant='body1' mt={4} textAlign='center' pb='40px'>
+        © 2023 All Rights Reserved
+      </Typography>
+    </Box>
+  );
+}
+
+export default Footer;
