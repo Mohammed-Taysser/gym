9a495fdc10e6c52967a66b330f225d76fe657e61
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-08-28 17:37:51 +0300

    add extra space for navbar

--- src/components/homepage/HeroBanner.tsx
+++ src/components/homepage/HeroBanner.tsx
@@ -5,39 +5,37 @@ import bannerImage from '../../assets/images/background/hero-banner.svg';
 
 function HeroBanner() {
   return (
-    <>
-      <Grid container className='banner' columnSpacing={2} rowSpacing={2}>
-        <Grid item md={6}>
-          <Typography color='primary' fontWeight='600' variant='h4'>
-            Fitness Club
-          </Typography>
+    <Grid container my={10} className='banner' columnSpacing={2} rowSpacing={2}>
+      <Grid item md={6}>
+        <Typography color='primary' fontWeight='600' variant='h4'>
+          Fitness Club
+        </Typography>
 
-          <Typography fontWeight={700} variant='h3' sx={{ my: 3 }}>
-            Sweat, Smile <br />
-            And Repeat
-          </Typography>
+        <Typography fontWeight={700} variant='h3' sx={{ my: 3 }}>
+          Sweat, Smile <br />
+          And Repeat
+        </Typography>
 
-          <Typography variant='h5' color='gray'>
-            Check out the most effective exercises personalized to you
-          </Typography>
+        <Typography variant='h5' color='gray'>
+          Check out the most effective exercises personalized to you
+        </Typography>
 
-          <Button
-            variant='contained'
-            size='large'
-            sx={{ mt: 3 }}
-            startIcon={<IoMdFitness />}
-            component={Link}
-            to='/exercises'
-          >
-            Explore Exercises
-          </Button>
-        </Grid>
-
-        <Grid item md={6}>
-          <img alt='hero-banner' src={bannerImage} className='banner-image' />
-        </Grid>
+        <Button
+          variant='contained'
+          size='large'
+          sx={{ mt: 3 }}
+          startIcon={<IoMdFitness />}
+          component={Link}
+          to='/exercises'
+        >
+          Explore Exercises
+        </Button>
       </Grid>
-    </>
+
+      <Grid item md={6}>
+        <img alt='hero-banner' src={bannerImage} className='banner-image' />
+      </Grid>
+    </Grid>
   );
 }
 
