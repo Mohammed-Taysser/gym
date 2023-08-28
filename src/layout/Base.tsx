1030b6fbbd7c041a99f50432ea7ef4227324576a
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-08-28 17:40:25 +0300

    update base layout

--- src/layout/Base.tsx
+++ src/layout/Base.tsx
@@ -1,24 +1,46 @@
-import { CircularProgress } from '@mui/material';
-import { Suspense } from 'react';
+import { Box, CircularProgress, CssBaseline } from '@mui/material';
+import { Suspense, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import AsyncWrapper from '../containers/AsyncWrapper';
 import { getExercises } from '../core/API';
 import { initApiCall } from '../redux/exercises.slice';
+import Footer from './Footer';
+import Navbar from './Navbar';
 
 function BaseLayout() {
   const dispatch = useDispatch();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }, [pathname]);
 
   const onExercisesFetch = (exercises: Exercise[]) => {
     dispatch(initApiCall(exercises));
   };
 
   return (
-    <AsyncWrapper<Exercise[]> apiCall={getExercises} setData={onExercisesFetch}>
-      <Suspense fallback={<CircularProgress />}>
-        <Outlet />
-      </Suspense>
-    </AsyncWrapper>
+    <Box sx={{ pt: 7 }}>
+      <CssBaseline />
+
+      <Navbar />
+
+      <AsyncWrapper<Exercise[]>
+        apiCall={getExercises}
+        setData={onExercisesFetch}
+      >
+        <Suspense fallback={<CircularProgress />}>
+          <Outlet />
+        </Suspense>
+      </AsyncWrapper>
+
+      <Footer />
+    </Box>
   );
 }
 
