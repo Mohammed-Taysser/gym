3cd070f550d0d06d19a630a87c9f359728c0ea2e
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-08-29 12:15:21 +0300

    create body parts page

---
+++ src/pages/public/BodyParts.tsx
@@ -0,0 +1,25 @@
+import { Container, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import Banner from '../../components/Banner';
+import BodyPartsGrid from '../../components/grids/BodyParts.grid';
+import { RootStoreState } from '../../redux/store';
+
+function BodyParts() {
+  const bodyParts = useSelector((state: RootStoreState) => state.api.bodyParts);
+
+  return (
+    <>
+      <Banner>
+        <Typography fontWeight={700} variant='h3' textAlign='center'>
+          All Body Part
+        </Typography>
+      </Banner>
+
+      <Container maxWidth='md' sx={{ my: 15 }}>
+        <BodyPartsGrid bodyParts={bodyParts} />
+      </Container>
+    </>
+  );
+}
+
+export default BodyParts;
