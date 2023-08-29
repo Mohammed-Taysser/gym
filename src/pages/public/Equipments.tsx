09fc1efb292e58ec3e2a33b108011304fbc33d9e
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-08-29 12:15:58 +0300

    create equipments page

---
+++ src/pages/public/Equipments.tsx
@@ -0,0 +1,27 @@
+import { Container, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import Banner from '../../components/Banner';
+import EquipmentsGrid from '../../components/grids/Equipments.grid';
+import { RootStoreState } from '../../redux/store';
+
+function Equipments() {
+  const equipments = useSelector(
+    (state: RootStoreState) => state.api.equipments
+  );
+
+  return (
+    <>
+      <Banner>
+        <Typography fontWeight={700} variant='h3' textAlign='center'>
+          All Equipments
+        </Typography>
+      </Banner>
+
+      <Container maxWidth='md' sx={{ my: 15 }}>
+        <EquipmentsGrid equipments={equipments} />
+      </Container>
+    </>
+  );
+}
+
+export default Equipments;
