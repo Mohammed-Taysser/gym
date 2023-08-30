ff180d38429e1b539bdc963c87bd586faf0bef61
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-08-30 16:18:54 +0300

    remove comments

--- src/core/API.ts
+++ src/core/API.ts
@@ -2,18 +2,10 @@ import axios from 'axios';
 
 // 66cee6e8edmsh2862291631dabcfp19961bjsn2169dd4cac3b
 
-// const youtubeOptions = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-//     'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
-//   },
-// };
-
 const exerciseInstance = axios.create({
   baseURL: 'https://exercisedb.p.rapidapi.com/exercises',
   headers: {
-    'X-RapidAPI-Key': 'dedf7f4b66msh0b3fbb8d65c52e7p163b32jsncd9f01be0161',
+    'X-RapidAPI-Key': '09a0988b78mshf28f3fed972c666p1a3175jsn86e98b42bd2b',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
 });
