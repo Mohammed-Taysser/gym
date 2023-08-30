8595af20afafce1aee61299ad173a8da05bdd79f
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-08-30 17:11:26 +0300

    create 404 page

--- src/pages/public/404.tsx
+++ src/pages/public/404.tsx
@@ -1,8 +1,17 @@
+import usePageTitle from '../../hooks/usePageTitle';
+import image from '../../assets/images/background/404.jpg'
+import { Container, Grid } from '@mui/material';
 
 function NotFound() {
-  return (
-    <div>NotFound</div>
-  )
+  usePageTitle(`404 - Page Not Found`);
+
+  return <Container>
+    <Grid container justifyContent='center' >
+      <Grid item md={6} >
+        <img src={image} alt='404' className='img-fluid' />
+      </Grid>
+    </Grid>
+  </Container>
 }
 
-export default NotFound
+export default NotFound;
