c74fb9dc675910828a538e0feae1523a5d0ee1cf
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-08-30 16:19:09 +0300

    add extra pages

--- src/core/Routes.tsx
+++ src/core/Routes.tsx
@@ -4,6 +4,9 @@ import Layout from '../layout/Base';
 
 const Homepage = lazy(() => import('../pages/public/Homepage'));
 const NotFound = lazy(() => import('../pages/public/404'));
+const Equipments = lazy(() => import('../pages/public/Equipments'));
+const BodyParts = lazy(() => import('../pages/public/BodyParts'));
+const TargetMuscles = lazy(() => import('../pages/public/TargetMuscles'));
 const ExerciseDetail = lazy(() => import('../pages/public/ExerciseDetail'));
 const BodyPartDetails = lazy(() => import('../pages/public/BodyPartDetails'));
 const EquipmentDetails = lazy(() => import('../pages/public/EquipmentDetails'));
@@ -30,12 +33,24 @@ const routes = createBrowserRouter([
         element: <Exercises />,
       },
       {
+        path: '/body-parts',
+        element: <BodyParts />,
+      },
+      {
         path: '/body-parts/:title',
         element: <BodyPartDetails />,
       },
       {
         path: '/target-muscles/:title',
         element: <TargetMuscleDetails />,
+      },
+      {
+        path: '/target-muscles/',
+        element: <TargetMuscles />,
+      },
+      {
+        path: '/equipments/',
+        element: <Equipments />,
       },
       {
         path: '/equipments/:title',
